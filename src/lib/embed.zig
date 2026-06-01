ce648463d2c32c1d1eebf818568b9ffc3994d125
Saturn Maoudis <saturnmaoudis@protonmail.com>
2026-06-01 15:17:35 +0100

    fix url whitespaces in names

--- src/lib/embed.zig
+++ src/lib/embed.zig
@@ -4,7 +4,11 @@ const zigcord = @import("zigcord");
 pub fn getUrl(allocator: std.mem.Allocator, hero: []const u8) ![]u8 {
     const segments = &[_][]const u8{ "https://github.com/AhhhSaturn/Archmother/blob/main/assets/", hero, ".png?raw=true" };
 
-    const url = try std.mem.join(allocator, "", segments);
+    const url_raw = try std.mem.join(allocator, "", segments);
+
+    const size = std.mem.replacementSize(u8, url_raw, " ", "%20");
+    const url = try allocator.alloc(u8, size);
+    _ = std.mem.replace(u8, url_raw, " ", "%20", url);
 
     return url;
 }
